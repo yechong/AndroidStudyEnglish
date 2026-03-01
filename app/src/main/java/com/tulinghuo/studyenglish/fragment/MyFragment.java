8581462891483502d6a6c5c73c81f3f2d643c00b
yechong <240542223@qq.com>
2026-03-01 22:54:18 +0800

    update

--- app/src/main/java/com/tulinghuo/studyenglish/fragment/MyFragment.java
+++ app/src/main/java/com/tulinghuo/studyenglish/fragment/MyFragment.java
@@ -1,16 +1,18 @@
 package com.tulinghuo.studyenglish.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
 import com.tulinghuo.studyenglish.R;
+import com.tulinghuo.studyenglish.activity.LoginActivity;
 
 public class MyFragment extends Fragment {
 
@@ -24,13 +26,18 @@ public class MyFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_my, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        RelativeLayout user_info = view.findViewById(R.id.user_info);
+        user_info.setOnClickListener(v -> {
+            Intent intent = new Intent(getActivity(), LoginActivity.class);
+            startActivity(intent);
+        });
     }
 }
